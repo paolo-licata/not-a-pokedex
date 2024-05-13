018ad6fec30816d86362ff7bfe5201a657badb81
Paolo Licata <paolo_licata@outlook.com>
2024-05-13 19:01:15 +0200

    First draft of the pokemonList
    If applied, this commit will have created a list of pokemons to be viewed in the app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-alert('Hello World');
-
-let favoriteFood = 'Chicken Wings';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, type: 'fire' },
+    { name: 'Squirtle', height: 0.5, type: 'water' },
+    { name: 'Diglett', height: 0.2, type: 'ground' },
+    { name: 'Pikachu', height: 0.4, type: 'electric' },
+    { name: 'Ekan', height: 2, type: 'poison' },
+    { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
+    { name: 'Jigglypuff', height: 0.5, type: ['fairy', 'normal'] },
+    { name: 'Paras', height: 0.3, type: ['grass', 'bug'] },
+    { name: 'Vulpix', height: 0.6, type: 'fire' },
+    { name: 'Seel', height: 1.1, type: 'water' },
+    { name: 'Eevee', height: 0.3, type: 'normal' },
+    { name: 'Hippopotas', height: 0.8, type: 'ground' }
+]
