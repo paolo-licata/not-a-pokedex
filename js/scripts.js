8b2c303b9fccb0c0759ef5b6e96c5cc37c60e03f
Paolo Licata <paolo_licata@outlook.com>
2024-05-15 18:53:56 +0200

    Created a loop to display all pokemons specifications. Styled a bit the content
    If this applies, the commit will create a list of pokemons, specifying some of their characteristics. The page will have been styled a little.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,23 @@ let pokemonList = [
     { name: 'Pikachu', height: 0.4, type: 'electric' },
     { name: 'Ekan', height: 2, type: 'poison' },
     { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
-    { name: 'Jigglypuff', height: 0.5, type: ['fairy', 'normal'] },
+    { name: 'Onix', height: 8.8, type: ['rock', 'ground'] },
     { name: 'Paras', height: 0.3, type: ['grass', 'bug'] },
     { name: 'Vulpix', height: 0.6, type: 'fire' },
-    { name: 'Seel', height: 1.1, type: 'water' },
+    { name: 'Snorlax', height: 2.8, type: 'normal' },
     { name: 'Eevee', height: 0.3, type: 'normal' },
-    { name: 'Hippopotas', height: 0.8, type: 'ground' }
+    { name: 'Wailmer', height: 2.6, type: 'water' }
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 1) {
+
+        document.write(pokemonList[i].name + ": " + " Height: " + pokemonList[i].height + " - This is a small pokemon" + "<br>" + "<br>");
+
+    } else if (pokemonList[i].height >= 1 && pokemonList[i].height < 2.5) {
+        document.write(pokemonList[i].name + " Height: " + pokemonList[i].height + " - This is an average-size pokemon" + "<br>" + "<br>");
+
+    } else {
+        document.write(pokemonList[i].name + " Height: " + pokemonList[i].height + " - That... is big!" + "<br>" + "<br>");
+    }
+}
