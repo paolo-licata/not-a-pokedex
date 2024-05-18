04de543af17febf0935b09e5d00ffb77ec038883
Paolo Licata <paolo_licata@outlook.com>
2024-05-18 18:37:50 +0200

    Created a new Repository variable to contain the IIFE. IIFE returns an object with 2 new functions: getAll and add
    If this applies, this commit will have established a new variable Repository and IIFE with return two function: getAll and add. forEach() function has been revisited again to allow the loop to cope with the change.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, type: 'fire' },
-    { name: 'Squirtle', height: 0.5, type: 'water' },
-    { name: 'Diglett', height: 0.2, type: 'ground' },
-    { name: 'Pikachu', height: 0.4, type: 'electric' },
-    { name: 'Ekan', height: 2, type: 'poison' },
-    { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
-    { name: 'Onix', height: 8.8, type: ['rock', 'ground'] },
-    { name: 'Paras', height: 0.3, type: ['grass', 'bug'] },
-    { name: 'Vulpix', height: 0.6, type: 'fire' },
-    { name: 'Snorlax', height: 2.8, type: 'normal' },
-    { name: 'Eevee', height: 0.3, type: 'normal' },
-    { name: 'Wailmer', height: 2.6, type: 'water' }
-]
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+        { name: 'Charmander', height: 0.6, type: 'fire' },
+        { name: 'Squirtle', height: 0.5, type: 'water' },
+        { name: 'Diglett', height: 0.2, type: 'ground' },
+        { name: 'Pikachu', height: 0.4, type: 'electric' },
+        { name: 'Ekan', height: 2, type: 'poison' },
+        { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
+        { name: 'Onix', height: 8.8, type: ['rock', 'ground'] },
+        { name: 'Paras', height: 0.3, type: ['grass', 'bug'] },
+        { name: 'Vulpix', height: 0.6, type: 'fire' },
+        { name: 'Snorlax', height: 2.8, type: 'normal' },
+        { name: 'Eevee', height: 0.3, type: 'normal' },
+        { name: 'Wailmer', height: 2.6, type: 'water' }
+    ]
 
-pokemonList.forEach(function (pokemon) {
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemonItem) {
+        pokemonList.add(pokemonItem);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
     console.log(pokemon.name + ' is ' + pokemon.height + ' meters tall.' + ' It is a type: ' + pokemon.type);
     document.write('<p>' + pokemon.name + ' is ' + pokemon.height + ' meters tall.' + ' It is a type: ' + pokemon.type + '</p>');
-
 });
+
 
 
 
